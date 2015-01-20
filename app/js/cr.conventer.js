19f9d783216782ff5caec63b0fd070184bc5f38d
ivanwong <team.iwong@gmail.com>
2015-01-20 22:03:30 +0300

    Add index view and write style fo it
     On branch views
     Changes to be committed:
    	modified:   app/jade/index.jade
    	modified:   app/jade/views/index.jade
    	modified:   app/js/cr.conventer.js
    	modified:   app/styl/theme.styl

--- app/js/cr.conventer.js
+++ app/js/cr.conventer.js
@@ -1,1 +1,13 @@
-angular.module('crConventer', ['ngRoute', 'app.controllers']);
+/* currency conventer v 0.0.1 by iwong */
+angular.module('crConventer', ['ngRoute', 'app.controllers'])
+	.config(function($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/index.html'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+
+			// inject $locationProvider and use $locationProvider.html5Mode(true); for nice url's
+	});
